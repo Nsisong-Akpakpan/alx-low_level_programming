19930267db02eac80a1606686b682c5ae215f72c
Nsisong-Akpakpan <nsisongakpakpan01@gmail.com>
2022-07-01 03:00:15 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
-/**
- *
- * * main - prints the size of various types
- *
- * * Return: 0 if exited properly, non-zero otherwise
- *
- * */
-
-
-
-int main(void)
-
+/
+ * main - Entry point
+ *  * Return: Always 0 (sucess)
+ *   */
+ int main(void)
 {
-
-		puts("\"Programming is like building a multilingual puzzle");
-           	return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
