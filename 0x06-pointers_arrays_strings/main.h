b0f7b3e59c290c6d709aacf7aca2229ceb7285b9
Nsisong-Akpakpan <nsisongakpakpan01@gmail.com>
2022-07-15 07:10:10 -0700

    A dream doesn't become reality through magic; it takes sweat, determination and hard work

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,7 +8,6 @@
  * char prototypes()
  */
 int _putchar(char c);
-int main(void);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -22,4 +21,5 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);  
 void print_buffer(char *b, int size);
-#endif
+
+#endif /*MAIN_H*/
