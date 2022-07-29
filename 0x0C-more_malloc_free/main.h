4ad1babd16992ec73d258f5cc3e6daadc85b751b
Nsisong-Akpakpan <nsisongakpakpan01@gmail.com>
2022-07-29 04:44:42 -0700

    _realloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,8 +6,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void _is_zero(char *argv[]);
-char *_initialize_array(char *ar, int lar);
-int _checknum(char *argv[], int n);
-int main(int argc, char *argv[]);
+int _strlen(char *);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int onlyNumbers(char *c);
+void multiply(char* s1, char* s2);
 #endif
